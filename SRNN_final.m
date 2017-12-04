d66dcbd50e05b3fd13703973579097e57f69560f
Jack Parker <jackmich@umich.edu>
2017-12-04 18:19:57 -0500

    SRNN

---
+++ SRNN_final.m
@@ -0,0 +1,92 @@
+%Short Recurrent Neural Network 
+
+%   input_matrix - input time series.
+%   purchase_output - feedback time series.
+
+X = tonndata(input_matrix,false,false);
+T = tonndata(purchase_output,false,false);
+
+% Choose a Training Function
+% 'trainlm' is usually fastest.
+% 'trainbr' takes longer but may be better for challenging problems.
+% 'trainscg' uses less memory. Suitable in low memory situations.
+trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
+
+% Create a Nonlinear Autoregressive Network with External Input
+inputDelays = 1:2;
+feedbackDelays = 1:2;
+hiddenLayerSize = 10;
+net = narxnet(inputDelays,feedbackDelays,hiddenLayerSize,'open',trainFcn);
+
+% Choose Input and Feedback Pre/Post-Processing Functions
+% Settings for feedback input are automatically applied to feedback output
+% For a list of all processing functions type: help nnprocess
+% Customize input parameters at: net.inputs{i}.processParam
+% Customize output parameters at: net.outputs{i}.processParam
+net.inputs{1}.processFcns = {'removeconstantrows','mapminmax'};
+net.inputs{2}.processFcns = {'removeconstantrows','mapminmax'};
+
+% Prepare the Data for Training and Simulation
+% The function PREPARETS prepares timeseries data for a particular network,
+% shifting time by the minimum amount to fill input states and layer
+% states. Using PREPARETS allows you to keep your original time series data
+% unchanged, while easily customizing it for networks with differing
+% numbers of delays, with open loop or closed loop feedback modes.
+[x,xi,ai,t] = preparets(net,X,{},T);
+
+% Setup Division of Data for Training, Validation, Testing
+% For a list of all data division functions type: help nndivide
+net.divideFcn = 'dividerand';  % Divide data randomly
+net.divideMode = 'time';  % Divide up every sample
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% Choose Plot Functions
+% For a list of all plot functions type: help nnplot
+net.plotFcns = {'plotperform','plottrainstate', 'ploterrhist', ...
+    'plotregression', 'plotresponse', 'ploterrcorr', 'plotinerrcorr'};
+
+% Train the Network
+[net,tr] = train(net,x,t,xi,ai);
+
+% Test the Network
+y = net(x,xi,ai);
+e = gsubtract(t,y);
+performance = perform(net,t,y)
+
+% Calculate Training, Validation and Test Performance
+trainTargets = gmultiply(t,tr.trainMask);
+valTargets = gmultiply(t,tr.valMask);
+testTargets = gmultiply(t,tr.testMask);
+trainPerformance = perform(net,trainTargets,y);
+valPerformance = perform(net,valTargets,y);
+testPerformance = perform(net,testTargets,y);
+
+%test results 
+testTargets_matrix = cell2mat(testTargets);
+number_entries = size(y,2);
+
+%store results
+test_results_actual = zeros(0,1);
+test_prediction = zeros(0,1);
+
+for i=1:number_entries
+    x = testTargets_matrix(1,i);
+    z = y(1,i);
+    
+    if (x == 0 | x == 1)
+        test_results_actual = [test_results_actual; x];
+        test_prediction = [test_prediction; z];
+    end
+    
+end
+
+%PREDICTED RESULTS USING SRNN (ONLY TEST RESULTS)
+test_prediction;
+
+%ACTUAL RESULTS (ONLY TEST RESULTS)
+test_results_actual;
+
+
+
