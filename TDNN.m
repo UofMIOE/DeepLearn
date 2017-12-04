ba99b29424cec6de47124dd05d12e179f8f5ec1b
jackmich <jackmich@umich.edu>
2017-12-03 20:44:20 -0500

    Simple TDNN that runs with test files

---
+++ TDNN.m
@@ -0,0 +1,44 @@
+% Time Delay Neural Network 
+
+
+
+num_ts = size(input_matrix,1); %total number of timestamps
+num_train = floor(num_ts * .75);  %records for training
+num_test = num_ts - num_train;   %records for testing
+
+% Partition 
+test_X = transpose(input_matrix(num_train+1:end,:));%input for testing
+test_T = transpose(purchase_output(num_train+1:end)); %targets for testing
+train_X = transpose(input_matrix(1:num_train,:));  %input set for training
+train_T = transpose(purchase_output(1:num_train,:)); %targets for training
+
+
+
+% Constructing NN
+nn = timedelaynet(1:3,12);
+nn = configure(nn,train_X, train_T);
+
+nn.layers{2}.transferFcn = 'tansig';
+
+train_X = con2seq(train_X);
+train_T = con2seq(train_T);
+test_X = con2seq(test_X);
+
+[Xs,Xi,Ai,Ts] = preparets(nn,train_X,train_T);
+
+% Training on NN
+nn = train(nn,Xs,Ts,Xi,Ai);
+view(nn)
+
+% Performance Test
+[Y,Xf,Af] = nn(Xs,Xi,Ai);
+perf = perform(nn,Ts,Y);
+
+% Calculate Prediction
+[netc,Xic,Aic] = closeloop(nn,Xf,Af);
+
+
+test_Y = netc(test_X,Xic,Aic);
+
+
+
